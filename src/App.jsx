4f58c4deb44fe4b7ad2508d784cc26d2ea5ca1f3
Nasrul Wahabi <wahabinasrul@gmail.com>
2025-07-19 17:24:20 +0700

    feat: add CORE_CONCEPTS data structure and integrate into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,12 +23,42 @@ function Header() {
   );
 }
 
+function CoreConcepts(props) {
+  return <li>
+    <img src={props.image} alt={props.title}></img>
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+}
+
+// Using destructuring to extract props (alternative).
+function CoreConceptsAlternative({ title, description, image }) {
+  return <li>
+    <img src={image} alt={title}></img>
+    <h3>{title}</h3>
+    <p>{description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* Shortcut, spread operator */}
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConceptsAlternative {...CORE_CONCEPTS[2]} />
+            <CoreConceptsAlternative {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
